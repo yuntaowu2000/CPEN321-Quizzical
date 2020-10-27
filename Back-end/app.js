b8e604a7b654b61dd198eb13c72c480a4407eafb
Ihsan Olawale <ihsanolawale@outlook.com>
2020-10-27 13:21:52 -0700

    Remove accidental redefinition of post

--- Back-end/app.js
+++ Back-end/app.js
@@ -45,7 +45,7 @@ MongoClient.connect(
   { useUnifiedTopology: true },
   (err, client) => {
   db = client.db("data");
-  var server = app.listen(8080, function() {
+  var server = app.listen(4000, function() {
     var port = server.address().port;
     console.log("Listening at %s", port);
   });  
@@ -182,55 +182,6 @@ app.get("/data", (req, res) => {
     });
 });
 */
-
-// used for testing http post in app
-http.createServer(function(req, res) {
-    let data = []
-    req.on('data', chunk =>
-    {
-        data.push(chunk)
-    })
-    req.on('end', ()=>
-    {
-        //print the values in the console
-        //data is in utf-8 format hexadecimals
-        //JSON.parse parses the data into human readable strings
-        var str = data.join("")
-        var obj
-        try
-        {
-            obj = JSON.parse(str)
-        } catch (ex)
-        {
-            obj = JSON.parse(str.replace(" ", ""))
-        }
-
-
-        console.log("UID: " + obj.uid)
-        console.log("Type: " + obj.type)
-
-        if (obj.type == "ProfileImage" || obj.type == "Profile Image")
-        {
-            fs.writeFileSync("username" + "_profile_img.jpg", obj.data, {encoding:'base64'});
-        }
-        else
-        {
-            fs.writeFileSync("username" + "_" + obj.type + ".txt", obj.data);
-            try
-            {
-                console.log(JSON.parse(obj.data))
-            } catch(ex)
-            {
-                console.log(obj.data)
-            }
-
-        }
-
-        // respond to the sender
-        res.statusCode = 200
-        res.end()
-    })
-}).listen(9090);
 
 // hardcoded get statement for winston's email
 http.createServer(function(req, res) {
