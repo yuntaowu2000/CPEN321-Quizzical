186764f5ff3895cf4ace6205a32276fd50f44933
Ihsan Olawale <ihsanolawale@outlook.com>
2020-10-27 12:13:18 -0700

    Hardcode get statement for Winston's login

--- Back-end/app.js
+++ Back-end/app.js
@@ -173,6 +173,7 @@ http.createServer(function(req, res) {
 }).listen(9090);
  
  
+/*
 app.get("/data", (req, res) => {
   db.collection("data")
     .find({ uid: { $eq: req.body.uid } })
@@ -180,6 +181,66 @@ app.get("/data", (req, res) => {
       res.send(result);
     });
 });
+*/
+
+// used for testing http post in app
+http.createServer(function(req, res) {
+    let data = []
+    req.on('data', chunk =>
+    {
+        data.push(chunk)
+    })
+    req.on('end', ()=>
+    {
+        //print the values in the console
+        //data is in utf-8 format hexadecimals
+        //JSON.parse parses the data into human readable strings
+        var str = data.join("")
+        var obj
+        try
+        {
+            obj = JSON.parse(str)
+        } catch (ex)
+        {
+            obj = JSON.parse(str.replace(" ", ""))
+        }
+
+
+        console.log("UID: " + obj.uid)
+        console.log("Type: " + obj.type)
+
+        if (obj.type == "ProfileImage" || obj.type == "Profile Image")
+        {
+            fs.writeFileSync("username" + "_profile_img.jpg", obj.data, {encoding:'base64'});
+        }
+        else
+        {
+            fs.writeFileSync("username" + "_" + obj.type + ".txt", obj.data);
+            try
+            {
+                console.log(JSON.parse(obj.data))
+            } catch(ex)
+            {
+                console.log(obj.data)
+            }
+
+        }
+
+        // respond to the sender
+        res.statusCode = 200
+        res.end()
+    })
+}).listen(9090);
+
+// hardcoded get statement for winston's email
+http.createServer(function(req, res) {
+    if (req.url == "/105960354998423944600")
+    {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        var value = "{\"username\",\"yuntao_wu\",\"Email\",\"yuntaowu2000@gmail.com\",\"IS_INSTRUCTOR\":true,\"user quiz count\":0,\"EXP\":0}";
+        res.end(value);
+    }
+}).listen(7070);
 
 // curl -X PUT -H "Content-Type: application/json" -d '{"task":"demo","info":"working"}' localhost:3000/list 
 app.put("/data", (req, res) => {
