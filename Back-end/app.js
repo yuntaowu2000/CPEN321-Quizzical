87b5722275ef8446c450f2d9f15a427d80daf990
Ihsan Olawale <ihsanolawale@outlook.com>
2020-10-27 08:21:20 -0700

    Prepare backend for deployment to server

--- Back-end/app.js
+++ Back-end/app.js
@@ -1,10 +1,29 @@
-const http = require("http");
-const fs = require("fs");
-
-var express = require("express"); // import Express module
+var createError = require('http-errors');
+var express = require('express');
 var MongoClient = require("mongodb").MongoClient;
-var app = express(); //Create Express object
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+var cors = require('cors');
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 MongoClient.connect(
   "mongodb://localhost:27017", 
@@ -121,5 +140,20 @@ app.delete("/data", function (req, res) {
     }
   );
 });
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
