6ba9dbdd6a248d029d8ebb6a961c73f135a08ea1
Andrew <shatterstorm34@gmail.com>
2020-10-28 04:16:35 -0700

    Getting a better layout for the create quiz

--- Front-end/app/src/main/java/com/cpen321/quizzical/quizactivities/CreateQuizActivity.java
+++ Front-end/app/src/main/java/com/cpen321/quizzical/quizactivities/CreateQuizActivity.java
@@ -30,14 +30,16 @@ import com.google.gson.JsonObject;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class CreateQuizActivity extends AppCompatActivity {
 
+    private LinearLayout questionCreateLayout;
     private ImageButton addQuestionButton;
     private List<String> questionList;
     private Spinner categoryList;
     private CheckBox questionHasPic;
-    private Button questionInputButton;
+    private Button answerInputButton;
     private String[] categories;
 
     private ImageButton takePictureButton;
@@ -54,6 +56,8 @@ public class CreateQuizActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_quiz);
+
+        questionCreateLayout = findViewById(R.id.question_create_layout);
 
         addQuestionButton = findViewById(R.id.add_question_button);
         addQuestionButton.setOnClickListener(v -> {addQuestionToList();});
@@ -88,49 +92,62 @@ public class CreateQuizActivity extends AppCompatActivity {
         questionHasPic = findViewById(R.id.question_has_pic);
         questionHasPic.setOnClickListener(v -> currHasPic = questionHasPic.isChecked());
 
-        questionInputButton = findViewById(R.id.question_input_button);
-        questionInputButton.setOnClickListener(v -> getQuestionInput());
+        answerInputButton = findViewById(R.id.answer_input_button);
+        answerInputButton.setOnClickListener(v -> addNewAnswer());
 
         takePictureButton = findViewById(R.id.take_picture_button);
         takePictureButton.setOnClickListener(v -> {
             Intent takePictureIntent = new Intent(this, TestPage.class);
             startActivity(takePictureIntent);});
 
+
+
     }
 
-    private void getQuestionInput() {
+    private void addNewAnswer() {
         //Todo: use a certain stackoverflow post to fix this layout.
+        String answer = "";
+        String pic = "";
+        final boolean[] isPic = {false};
+
         LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
         layoutParams.setMargins(10, 5, 10, 5);
+
         LinearLayout layout = new LinearLayout(this);
         layout.setOrientation(LinearLayout.VERTICAL);
 
-        TextView questionText = new TextView(this);
-        questionText.setText("Enter string for question");
-        questionText.setLayoutParams(layoutParams);
 
-        EditText questionInput = new EditText(this);
-        questionInput.setLayoutParams(layoutParams);
-        questionInput.setHint("What is 1+1");
-        questionInput.setInputType(InputType.TYPE_CLASS_TEXT);
-        questionInput.setText(currQuestion);
-        questionInput.setMaxLines(1);
+        TextView answerText = new TextView(this);
+        answerText.setText("Enter answer");
+        answerText.setLayoutParams(layoutParams);
 
-        TextView picText = new TextView(this);
-        picText.setText("Enter string for pic");
-        picText.setLayoutParams(layoutParams);
+        EditText answerInput = new EditText(this);
+        answerInput.setLayoutParams(layoutParams);
+        answerInput.setHint("What is 1+1");
+        answerInput.setInputType(InputType.TYPE_CLASS_TEXT);
+        answerInput.setText(answer);
+        answerInput.setMaxLines(1);
 
-        EditText picInput = new EditText(this);
-        picInput.setLayoutParams(layoutParams);
-        picInput.setHint("https://raw.githubusercontent.com/yuntaowu2000/testUploadModels/master/006.png");
-        picInput.setInputType(InputType.TYPE_CLASS_TEXT);
-        picInput.setText(currPicSrc);
-        picInput.setMaxLines(1);
+        ImageButton answerPic = new ImageButton(this);
+        answerPic.setImageResource(android.R.drawable.ic_menu_camera);
+        answerPic.setOnClickListener(v -> {
+            Intent takePictureIntent = new Intent(this, TestPage.class);
+            startActivity(takePictureIntent);});
 
-        layout.addView(questionText);
-        layout.addView(questionInput);
-        layout.addView(picText);
-        layout.addView(picInput);
+        CheckBox checkBox = findViewById(R.id.question_has_pic);
+        checkBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isPic[0] = checkBox.isChecked();
+                answerPic.setVisibility(checkBox.isChecked()?View.GONE:View.VISIBLE);
+                answerInput.setVisibility(checkBox.isChecked()?View.VISIBLE:View.GONE);
+            }
+        });
+
+        layout.addView(answerText);
+        layout.addView(checkBox);
+        layout.addView(answerInput);
+        layout.addView(answerPic);
 
         AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this).setTitle("Question input")
                 .setView(layout)
@@ -141,10 +158,10 @@ public class CreateQuizActivity extends AppCompatActivity {
 
         alertDialog.getButton(DialogInterface.BUTTON_POSITIVE).setOnClickListener(v ->
         {
-            currQuestion = questionInput.getText().toString();
-            currPicSrc = picInput.getText().toString();
+            currQuestion = answerInput.getText().toString();
             alertDialog.dismiss();
         });
+        // create new row here
     }
 
     private void addQuestionToList() {
