42f30ea63e7dd940014420cce524ec111bf6fc04
Ihsan Olawale <ihsanolawale@outlook.com>
2020-11-01 15:12:29 -0800

    Add quiz route to app.js

--- Back-end/app.js
+++ Back-end/app.js
@@ -6,6 +6,7 @@ var logger = require("morgan");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var uploadRouter = require("./routes/upload");
+var quizRouter = require("./routes/quiz");
 
 var app = express();
 
@@ -21,7 +22,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
-app.use("/upload", uploadRouter);
+app.use("/quiz", uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
