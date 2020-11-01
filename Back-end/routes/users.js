a88216d65c31326e45a2c2f9f87c6727dc18a058
OhmaGiardan <54877392+OhmaGiardan@users.noreply.github.com>
2020-11-01 15:08:50 -0800

    removed quiz, moving to quiz.js

--- Back-end/routes/users.js
+++ Back-end/routes/users.js
@@ -93,15 +93,6 @@ router.get("/", (req, res, next) => {
       }
     });
   }
-  else if (type === "quiz") {
-    db.collection("quizzes").find({ $and: [ {"uid": uid}, {"data.id": quiz_code} ] }).toArray((err, frequency) => {
-      if (err) {
-	throw err;
-      } else {
-	res.send(frequency);
-      }
-    });
-  }
 });
 
 module.exports = router;
