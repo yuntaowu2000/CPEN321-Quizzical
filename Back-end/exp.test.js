e86873e4c0aaed9519448bbf8129976c34a2513d
OhmaGiardan <54877392+OhmaGiardan@users.noreply.github.com>
2020-11-03 16:50:14 -0800

    jest mock test

---
+++ Back-end/exp.test.js
@@ -0,0 +1,10 @@
+test("test EXP calculation", () => {
+  const mock = jest.fn((likes) => 10 + 20*likes);
+
+  let result = mock(5);
+
+  expect(result).toBe(110);
+  expect(mock).toHaveBeenCalled();
+  expect(mock).toHaveBeenCalledTimes(1);
+  expect(mock).toHaveBeenCalledWith(5);
+});
