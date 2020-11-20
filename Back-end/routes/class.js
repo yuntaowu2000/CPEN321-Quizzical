3456086e7f2c84108f03a4b986f6cb03d50c8b20
Ihsan Olawale <ihsanolawale@outlook.com>
2020-11-19 18:59:07 -0800

    Comment out partial implementation to keep server functional

--- Back-end/routes/class.js
+++ Back-end/routes/class.js
@@ -79,6 +79,7 @@ function handleDeleteClass(isInstructor, classCode, uid) {
         }
     });
 
+    /*
     classDb.collection("class" + classCode).find().project({_id:0,uid:1}).forEach((doc) => {
       let classList = {};
       db.collection().find({uid: {$eq: doc.uid}}).project({_id:0,classList:1}).toArray((result) => {
@@ -88,6 +89,8 @@ function handleDeleteClass(isInstructor, classCode, uid) {
       classList = JSON.stringify(classList);
       db.collection().updateOne({uid: {$eq: doc.uid}}, {$set: {classList: classList}});
     });
+    */
+
     classDb.collection("class" + classCode).drop((err, delOK) => {
       if (err) {
         throw err;
