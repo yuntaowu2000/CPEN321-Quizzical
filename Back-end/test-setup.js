e3d94cfa9279fd9afe01bde3fb5fec4fec36a8a2
yuntaowu2000 <49249544+yuntaowu2000@users.noreply.github.com>
2020-11-29 14:33:40 -0800

    try fix travis warnings

--- Back-end/test-setup.js
+++ Back-end/test-setup.js
@@ -27,7 +27,7 @@ module.exports = {
     // Connect to Mongoose
     beforeAll(async () => {
       const url = `mongodb://127.0.0.1/${databaseName}`;
-      await mongoose.connect(url, { useNewUrlParser: true });
+      await mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true});
     });
 
     // Cleans up database between each test
