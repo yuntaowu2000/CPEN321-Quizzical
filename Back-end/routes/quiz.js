15e07b1525b1bf8c1b8b0bddda622cc03f5f11d2
Ihsan Olawale <ihsanolawale@outlook.com>
2020-12-01 10:36:39 -0800

    Wrap logic in else inside studentWrongCounts

--- Back-end/routes/quiz.js
+++ Back-end/routes/quiz.js
@@ -164,28 +164,30 @@ router.get("/studentWrongCounts", (req, res, next) => {
     .find({$and: [{classCode}, {quizCode}]})
     .project({_id:0, questionList: 1})
     .toArray((err, data) => {
+      console.error(data);
       if (data.length === 0) {
         res.send("");
-      }
-      var questionList = data[0]["questionList"];
-      var len = questionList.length;
+      } else {
+        var questionList = data[0]["questionList"];
+        var len = questionList.length;
 
-      classesDb.collection(classDbName).find({["quiz" + quizCode + "wrongQuestionIds"]: {$ne: null}})
-      .project({_id:0, ["quiz" + quizCode + "wrongQuestionIds"]: 1})
-      .toArray((err, wrongIds) => {
-        let count = [];
-        for (var i = 0; i < len; i++) {
-          count.push(0);
-        }
-        for (var d of wrongIds) {
-          let val = Object.values(d)[0];
-          let currIds = val.substring(1, val.length - 1).split(",");
-          for (var id of currIds) {
-            count[Number(id) - 1] += 1;
-          }
-        }
-        res.send(count);
-      });
+        classesDb.collection(classDbName).find({["quiz" + quizCode + "wrongQuestionIds"]: {$ne: null}})
+          .project({_id:0, ["quiz" + quizCode + "wrongQuestionIds"]: 1})
+          .toArray((err, wrongIds) => {
+            let count = [];
+            for (var i = 0; i < len; i++) {
+              count.push(0);
+            }
+            for (var d of wrongIds) {
+              let val = Object.values(d)[0];
+              let currIds = val.substring(1, val.length - 1).split(",");
+              for (var id of currIds) {
+                count[Number(id) - 1] += 1;
+              }
+            }
+            res.send(count);
+          });
+      }
   });
 
 });
