feba3f0189b8557b5d04523d508a5165613e8d8f
yuntaowu2000 <49249544+yuntaowu2000@users.noreply.github.com>
2020-12-01 12:11:48 -0800

    added quiz module in class test

--- Back-end/test/class.test.js
+++ Back-end/test/class.test.js
@@ -46,3 +46,46 @@ describe("class test", () => {
     });
   
 });
+
+describe("class quiz module test", () => {
+  
+  beforeAll(async() => {
+    var client = await MongoClient.connect("mongodb://localhost:27017",  {useNewUrlParser: true, useUnifiedTopology: true});
+    var classDb = await client.db("data");
+
+    await classDb.collection("classInfo").insertOne({"classCode" : 1, "uid" : "1", "category" : "Math", "className" : "testClass1", "instructorUID" : "1", "quizModules" : "{\"category\":\"Math\",\"classCode\":1,\"id\":0,\"moduleName\":\"module1\"}"});
+
+    await classDb.collection("classInfo").insertOne({"classCode" : 2, "uid" : "2", "category" : "English", "className" : "testClass2", "instructorUID" : "2", "quizModules" : "{\"category\":\"English\",\"classCode\":2,\"id\":0,\"moduleName\":\"module1\"}"});
+
+    await client.close();
+  });
+
+  afterAll(async() => {
+    var client = await MongoClient.connect("mongodb://localhost:27017",  {useNewUrlParser: true, useUnifiedTopology: true});
+    var db = await client.db("data");
+    await db.dropCollection("classInfo");
+    await db.close();
+    await client.close();
+  });
+
+  // test GET of "/" and "/studentWrongCounts"
+  test("get general class info", async (done) => {
+      let response = await request.get("/classes").query({classCode: "2", type: "quizModules"});
+      expect(response.status).toBe(200);
+      done();
+  });
+
+  test("get general class info 2", async (done) => {
+      let response = await request.get("/classes").query({classCode: "1", type: "quizModules"});
+      expect(response.status).toBe(200);
+      done();
+  });
+
+  test("get general class info with class undefined", async (done) => {
+      let response = await request.get("/classes").query({classCode: "11111", type: "quizModules"});
+      expect(response.status).toBe(200);
+      done();
+  });
+
+});
+
