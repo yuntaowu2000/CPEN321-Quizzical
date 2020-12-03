f8e10aaf1b93f7073527f021b6c39222d3e67d96
yuntaowu2000 <49249544+yuntaowu2000@users.noreply.github.com>
2020-12-02 19:19:41 -0800

    Update InstructorCreateClassTest.java

--- Front-end/app/src/androidTest/java/com/cpen321/quizzical/InstructorCreateClassTest.java
+++ Front-end/app/src/androidTest/java/com/cpen321/quizzical/InstructorCreateClassTest.java
@@ -23,7 +23,7 @@ public class InstructorCreateClassTest {
     /*Note: if you want to run the tests on your own machine,
         you may need to replace the client id with your own google client id at line 108 in CPEN321-Quizzical\Front-end\app\src\main\res\values\strings.xml
         and sign in the app properly.
-        You need an instructor account for the tests here.
+        You need an instructor account for the tests here and with some class already set up.
         */
     @Rule
     public ActivityTestRule<HomeActivity> activityTestRule = new ActivityTestRule<HomeActivity>(HomeActivity.class);
